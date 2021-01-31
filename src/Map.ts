affc1fb7a600412c2f0109ab81382b0052f53a70
mariam-montaser <mariammontaser2018@gmail.com>
2021-01-31 09:28:42 +0200

    intial google maps clone

---
+++ src/Map.ts
@@ -0,0 +1,105 @@
+
+// Instructions to every other class
+// on how they can be an argument to 'addMarker' method
+export interface Mappable {
+    location: {
+        lat: number;
+        lng: number;
+    };
+    markerContent(): string;
+}
+
+export class Map {
+    private googleMap: google.maps.Map;
+    
+    constructor(divId: string) {
+        this.googleMap = new google.maps.Map(document.getElementById(divId), {
+            zoom: 1,
+            center: {
+                lat: 0,
+                lng: 0
+            }
+        })
+    }
+
+    addMarker(mappable: Mappable): void {
+        const marker = new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: mappable.location.lat,
+                lng: mappable.location.lng
+            }
+        });
+
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: mappable.markerContent()
+            })
+            infoWindow.open(this.googleMap, marker)
+        })
+    }
+
+}
+
+
+
+// export class Map {
+//     private googleMap: google.maps.Map;
+    
+//     constructor(divId: string) {
+//         this.googleMap = new google.maps.Map(document.getElementById(divId), {
+//             zoom: 1,
+//             center: {
+//                 lat: 0,
+//                 lng: 0
+//             }
+//         })
+//     }
+
+//     addMarker(Mappable: User | Company): void {
+//         new google.maps.Marker({
+//             map: this.googleMap,
+//             position: {
+//                 lat: Mappable.location.lat,
+//                 lng: Mappable.location.lng
+//             }
+//         })
+//     }
+
+// }
+
+
+
+// export class Map {
+//     private googleMap: google.maps.Map;
+    
+//     constructor(divId: string) {
+//         this.googleMap = new google.maps.Map(document.getElementById(divId), {
+//             zoom: 1,
+//             center: {
+//                 lat: 0,
+//                 lng: 0
+//             }
+//         })
+//     }
+
+//     addUserMarker(user: User): void {
+//         new google.maps.Marker({
+//             map: this.googleMap,
+//             position: {
+//                 lat: user.location.lat,
+//                 lng: user.location.lng
+//             }
+//         })
+//     }
+
+//     addCompanyMarker(company: Company): void {
+//         new google.maps.Marker({
+//             map: this.googleMap,
+//             position: {
+//                 lat: company.location.lat,
+//                 lng: company.location.lng
+//             }
+//         })
+//     }
+// }
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { User } from "./User";
+import { Company } from "./Company";
+import { Map } from "./Map";
+
+
+const user = new User();
+const company = new Company();
+const map = new Map('map');
+
+map.addMarker(user);
+map.addMarker(company);
+
+// map.addUserMarker(user);
+// map.addCompanyMarker(company);
